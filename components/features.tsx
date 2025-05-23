4b4a82ba3b28d49b4b6a20adb0bbbbfac0a513ad
aogajoseph <aogathesecond@gmail.com>
2025-05-23 16:23:14 +0300

    Update: Polish homepage content

--- components/features.tsx
+++ components/features.tsx
@@ -48,7 +48,7 @@ export default function Features() {
           </div>
           <div className="flex justify-center pb-0 md:pb-0 -mb-2" data-aos="fade-up">
             <Image
-              className="inline-flex"
+              className="inline-flex rounded-xl"
               src={FeaturesImage}
               alt="Features"
               width={760}
